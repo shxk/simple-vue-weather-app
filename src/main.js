14c6b708ac6b92b9dfdf5243d24e0fc705d7c3fa
Shak <shak.developer@gmail.com>
2019-05-16 20:31:34 +0100

    failed build fix?

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,6 +7,7 @@ import Vuetify from 'vuetify'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(bootstrap)
 Vue.use(Vuetify, {
